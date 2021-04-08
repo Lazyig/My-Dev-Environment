0b1264fa651b968b579dcd1fe6c32eb8ad92586c
19 <kimig009@gmail.com>
2021-04-08 08:43:31 +0000

    Update Git/git_setting.sh

---
+++ Git/git_setting.sh
@@ -0,0 +1,8 @@
+git config --global user.email kimig009@gmail.com
+git config --global user.name lazyig
+git config --global user.signingkey 5FFE7324D4895B40B7208D102503FBFC8F6D97CC
+git config --global commit.gpgsign true
+git config --global core.commentChar ";"
+git config --global alias.log2 "log --oneline --graph --decorate"
+git config --global alias.merge2 "merge --no-ff"
+git config --global alias.smerge "merge --squash"
