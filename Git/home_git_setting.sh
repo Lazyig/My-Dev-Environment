1391af51f264e26fcdb73e353a96b8035487f8a6
19 <kimig009@gmail.com>
2021-09-08 12:43:20 +0000

    Update home_git_setting.sh

--- Git/home_git_setting.sh
+++ Git/home_git_setting.sh
@@ -3,6 +3,7 @@ git config --global user.name lazyig
 git config --global user.signingkey 5FFE7324D4895B40B7208D102503FBFC8F6D97CC
 git config --global commit.gpgsign true
 git config --global core.commentChar ";"
+git config --global core.editor "vim"
 git config --global init.defaultBranch main
 git config --global alias.log2 "log --oneline --graph --decorate"
 git config --global alias.merge2 "merge --no-ff"
