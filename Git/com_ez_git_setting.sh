f90b904ceb2cf57e872678cba9dc2eaf68b0acc0
19 <kimig009@gmail.com>
2021-12-14 03:58:43 +0000

    Update Git/com_ez_git_setting.sh

---
+++ Git/com_ez_git_setting.sh
@@ -0,0 +1,13 @@
+git config --global user.email kim19@ezinc.co.kr
+git config --global user.name ilguKim
+git config --global core.commentChar ";"
+git config --global core.editor "vim"
+git config --global credential.helper "store"
+git config --global alias.log2 "log --oneline --graph --decorate"
+git config --global alias.merge2 "merge --no-ff"
+git config --global alias.smerge "merge --squash"
+git config --global alias.sdiff "diff --name-only"
+git config --global alias.sdiffs "diff --name-only --staged"
+git config --global alias.sdiffc "diff --name-only --cached"
+git config --global alias.diffs "diff --staged"
+git config --global alias.diffc "diff --cached"
