c351575a8ae875b7d9f88f5d115741686c8ecc07
19 <kimig009@gmail.com>
2022-01-20 23:28:08 +0000

    Update Git/home_git_setting.sh, Git/com_ez_git_setting.sh, Git/common.sh

---
+++ Git/common.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+git config --global core.editor "vim"
+git config --global core.commentChar ";"
+git config --global alias.log2 "log --oneline --graph --decorate"
+git config --global alias.log3 "log --format='[%H]%s'"
+git config --global alias.merge2 "merge --no-ff"
+git config --global alias.smerge "merge --squash"
+git config --global alias.sdiff "diff --name-only"
+git config --global alias.sdiffs "diff --name-only --staged"
+git config --global alias.sdiffc "diff --name-only --cached"
+git config --global alias.diffs "diff --staged"
+git config --global alias.diffc "diff --cached"
--- Git/home_git_setting.sh
+++ Git/home_git_setting.sh
@@ -1,15 +1,7 @@
+#!/bin/sh
 git config --global user.email kimig009@gmail.com
 git config --global user.name lazyig
 git config --global user.signingkey 5FFE7324D4895B40B7208D102503FBFC8F6D97CC
 git config --global commit.gpgsign true
-git config --global core.commentChar ";"
-git config --global core.editor "vim"
 git config --global init.defaultBranch main
-git config --global alias.log2 "log --oneline --graph --decorate"
-git config --global alias.merge2 "merge --no-ff"
-git config --global alias.smerge "merge --squash"
-git config --global alias.sdiff "diff --name-only"
-git config --global alias.sdiffs "diff --name-only --staged"
-git config --global alias.sdiffc "diff --name-only --cached"
-git config --global alias.diffs "diff --staged"
-git config --global alias.diffc "diff --cached"
+
